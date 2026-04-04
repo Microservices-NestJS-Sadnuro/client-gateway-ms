9ad9606270b3dac9ba71f779b1a1f2021e51ab42
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-04 01:14:07 -0500

    feat: integrate microservices client and configure product service connection via TCP

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,12 +3,15 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
+    PRODUCTS_MS_HOST: string;
+    PRODUCTS_MS_PORT: number;
 }
 
 const envsSchema = joi.object({
-    PORT: joi.number().required()
-})
-    .unknown(true);
+    PORT: joi.number().required(),
+    PRODUCTS_MS_HOST: joi.string().required(),
+    PRODUCTS_MS_PORT: joi.number().required(),
+}).unknown(true);
 
 const { error, value } = envsSchema.validate(process.env);
 
@@ -20,4 +23,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
+    productsMsHost: envVars.PRODUCTS_MS_HOST,
+    productsMsPort: envVars.PRODUCTS_MS_PORT
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, PRODUCT_SERVICE } from 'src/config';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
+  // Registrar microservicio de destino y protocolo de comunicacion
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productsMsHost,
+          port: envs.productsMsPort
+        }
+      },
+    ])
+  ]
 })
 export class ProductsModule { }
