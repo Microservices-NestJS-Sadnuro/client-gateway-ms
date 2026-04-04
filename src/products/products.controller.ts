8b9aefa070d546ba260de5b57748572a41b6ec80
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-04 01:29:47 -0500

    feat: inject product microservice client into products controller and update findAll to proxy request

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PRODUCT_SERVICE } from 'src/config';
 
 @Controller('products')
 export class ProductsController {
-  constructor() { }
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy
+  ) { }
 
   @Post()
   create() {
@@ -11,7 +15,7 @@ export class ProductsController {
 
   @Get()
   findAll() {
-    return 'Find all products';
+    return this.productsClient.send({ cmd: 'find_all_products' }, {});
   }
 
   @Get(':id')
