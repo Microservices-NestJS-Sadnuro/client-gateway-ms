58c93c5d608c1177e2cab7815619800c0a4604de
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-04 12:32:50 -0500

    feat: refactor orders controller to use microservice client proxy for all endpoints

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,25 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, Query, ParseIntPipe } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { ORDERS_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common';
+import { catchError } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) { }
+  constructor(
+    @Inject(ORDERS_SERVICE) private readonly ordersClient: ClientProxy
+  ) { }
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+    console.log('Entry REST Controller - create order');
+    return this.ordersClient.send('createOrder', createOrderDto);
   }
 
   @Get()
-  findAll() {
-    return this.ordersService.findAll();
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.ordersClient.send('findAllOrders', paginationDto); ///this.ordersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    return this.ordersClient.send('findOne', { id })
+      .pipe( // Implement observable pipe for chatching exception
+        catchError(err => { throw new RpcException(err) })
+      );; //this.ordersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  changeOrderStatus(@Query('id', ParseIntPipe) id: number, @Body() updateOrderDto: UpdateOrderDto) {
+    return this.ordersClient.send('changeOrderStatus', { id, ...updateOrderDto })
+
   }
 
 }
