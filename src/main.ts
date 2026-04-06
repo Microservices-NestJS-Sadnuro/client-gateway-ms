30dc747cef6ad06f0bf652fd81a6b6b74f9ca0b5
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-05 21:11:07 -0500

    feat: integrate Swagger OpenAPI documentation for the client gateway API

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('ClientGateway');
@@ -16,8 +17,30 @@ async function bootstrap() {
     })
   );
   app.useGlobalFilters(new RpcCustomExceptionFilter());
-  await app.listen(envs.port);
 
+  // Configuración para el documento OpenAPI
+  const swaggerDocConfig = new DocumentBuilder()
+    .setTitle('Client Gateway API')
+    .setDescription('Documentación de la API del Client Gateway')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  // Generacion de documento OpenAPI
+  const document = SwaggerModule.createDocument(app, swaggerDocConfig, {
+    operationIdFactory: (_controllerKey, methodKey) => methodKey,
+    autoTagControllers: true
+  });
+
+  // Publicación de Swagger UI
+  SwaggerModule.setup('docs', app, document, {
+    useGlobalPrefix: true,
+    customSiteTitle: 'Client Gateway API',
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  })
+  await app.listen(envs.port);
   logger.log(`Client Gateway is running on port ${envs.port}`);
 }
 bootstrap();
